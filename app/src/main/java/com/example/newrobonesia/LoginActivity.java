bcd91c83472c27cb21d069333c75b1af12dfe18f
nendi123 <nendi@niagateknologi.net>
2021-02-09 07:55:32 +0700

    Tambah Library connect to api. tapi api nya tidak bisa di akses

--- app/src/main/java/com/example/newrobonesia/LoginActivity.java
+++ app/src/main/java/com/example/newrobonesia/LoginActivity.java
@@ -8,10 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin,btnRegister;
     EditText editEmail, editPassword;
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +37,20 @@ public class LoginActivity extends AppCompatActivity {
         editEmail = (EditText) findViewById(R.id.txtEmail);
         editPassword = (EditText) findViewById(R.id.txtPassword);
 
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                String post(String url, String json) throws IOException {
+//                    RequestBody body = RequestBody.create(JSON, json);
+//                    Request request = new Request.Builder()
+//                            .url(url)
+//                            .post(body)
+//                            .build();
+//                    try (Response response = client.newCall(request).execute()) {
+//                        return response.body().string();
+//                    }
+//                }
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
