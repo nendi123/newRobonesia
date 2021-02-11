d833d1092172c19e2d23e84fc53a80a0c5dc8076
nendi123 <nendi@niagateknologi.net>
2021-02-11 07:46:53 +0700

    Penerapan api pada LoginActivity.java

---
+++ app/src/main/java/com/example/newrobonesia/Constants.java
@@ -0,0 +1,5 @@
+package com.example.newrobonesia;
+
+public class Constants {
+    public final String BASE_URL = "https://robonesiaone.online/api/v1/";
+}
--- app/src/main/java/com/example/newrobonesia/LoginActivity.java
+++ app/src/main/java/com/example/newrobonesia/LoginActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
-import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -30,8 +36,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnRegister = (Button) findViewById(R.id.btnRegister);
         editEmail = (EditText) findViewById(R.id.txtEmail);
@@ -41,16 +45,39 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                String post(String url, String json) throws IOException {
-//                    RequestBody body = RequestBody.create(JSON, json);
-//                    Request request = new Request.Builder()
-//                            .url(url)
-//                            .post(body)
-//                            .build();
-//                    try (Response response = client.newCall(request).execute()) {
-//                        return response.body().string();
-//                    }
-//                }
+                MediaType MEDIA_TYPE = MediaType.parse("application/json");
+                Constants constants = new Constants();
+                OkHttpClient client = new OkHttpClient();
+                JSONObject postData = new JSONObject();
+
+                try {
+                    postData.put("email", editEmail.getText().toString());
+                    postData.put("password", editPassword.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                RequestBody requestBody = RequestBody.create(MEDIA_TYPE, postData.toString());
+                Request request = new Request.Builder()
+                        .url(constants.BASE_URL)
+                        .post(requestBody)
+                        .header("Accept", "application/json")
+                        .header("Content-Type", "application/json")
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        String mMessage = e.getMessage().toString();
+                        Log.e("failure Response", mMessage);
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        String mMessage = response.body().string();
+                        Log.w("Sukses", mMessage);
+                    }
+                });
+
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
